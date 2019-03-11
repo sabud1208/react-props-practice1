853902e6f3e99eea68a97c0c9f93da2bc03f5dfc
sabud1208 <43481963+sabud1208@users.noreply.github.com>
2019-03-11 15:16:50 -0400

    different image way

--- src/Image.js
+++ src/Image.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import YonceBlake from './YonceBlake.jpg'
 
-
+console.log(YonceBlake)
 class Image extends React.Component{
  render(){
-   return <img src={this.props.url} alt = ""/>;
+   return <img src={YonceBlake} alt = ""/>;
  }
 }
 
